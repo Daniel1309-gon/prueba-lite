29a154cde414132b2858a3063e9904482413ed24
Daniel Gonzalez <dgonzalezca@unal.edu.co>
2025-12-29 21:46:06 -0500

    settings para deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*sh#qe(csy^(zs-as531s&q11$o-ra-)m+m^$&teaxxzen)dz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
